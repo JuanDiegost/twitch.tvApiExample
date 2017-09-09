f5a02451ffcc64800677ba4c4302c3493ade673a
JuanDiegost <molinajuan149@gmail.com>
2017-09-09 09:35:14 -0500

    Agregado responsive, y filtrado por estado del canal

--- js/vendor.js
+++ js/vendor.js
@@ -8,6 +8,24 @@ $(document).ready(function() {
     addChanel(channels[i]);
   }
 
+  $("#all").on("click", function(event) {
+    event.preventDefault();
+    $("#chanels").removeClass("online-v");
+    $("#chanels").removeClass("Offline");
+  });
+
+  $("#online").on("click", function(event) {
+    event.preventDefault();
+    $("#chanels").addClass("online-v");
+    $("#chanels").removeClass("Offline-v");
+  });
+
+  $("#offline").on("click", function(event) {
+    event.preventDefault();
+    $("#chanels").removeClass("online-v");
+    $("#chanels").addClass("Offline-v");
+  });
+
   function addChanel(channelName) {
     $.ajax({
       url: api +"streams" +'/'+ channelName+end,
@@ -16,12 +34,11 @@ $(document).ready(function() {
         console.log(data);
         if (data["stream"] == null)
         {
-        $('#chanels').append('<article class="chanel divider-color"><img class="logo" src="'+'https://dummyimage.com/50x50/ecf0e7/5c5457.jpg&text=0x3F'
+        $('#chanels').append('<article class="chanel divider-color offline-a"><img class="logo" src="'+'https://dummyimage.com/50x50/ecf0e7/5c5457.jpg&text=0x3F'
 +'" alt=""><h3 class="title">'+channelName+'</h3><h5 class="description">'+'Offline'+'</h5></article>');
         } else {
         var channel=data.stream.channel;
-          console.log(channel.display_name+' no online');
-          $('#chanels').append('<article id="'+channel.display_name+'" class="chanel divider-color"><img class="logo" src="'+channel.logo+'" alt=""><h3 class="title">'+channel.display_name+'</h3><h5 class="description">'+channel.status+'</h5><span class="fa fa-play preview play"></span><img class="preview" src="'+data.stream.preview.large+'" alt=""><div class="container-btn"><a class="fa fa-external-link btnS text-primary-color accent-color divider-color box-shadow " href="'+channel.url+'" target="_blank"><span>Go</span></a><button class="btnS divider-color accent-color box-shadow" id="btn-preview-'+channel.display_name+'" type="button" name="button">Preview</button></div></article>');
+          $('#chanels').append('<article id="'+channel.display_name+'" class="chanel divider-color online-a"><img class="logo" src="'+channel.logo+'" alt=""><h3 class="title">'+channel.display_name+'</h3><h5 class="description">'+channel.status+'</h5><span class="fa fa-play preview play"></span><img class="preview" src="'+data.stream.preview.large+'" alt=""><div class="container-btn"><a class="fa fa-external-link btnS text-primary-color accent-color divider-color box-shadow " href="'+channel.url+'" target="_blank"><span>Go</span></a><button class="btnS divider-color accent-color box-shadow" id="btn-preview-'+channel.display_name+'" type="button" name="button">Preview</button></div></article>');
           $('#btn-preview'+channel.display_name).on("click", function(event) {
               $('#'+channel.display_name).toggleClass( "view" );
             }
